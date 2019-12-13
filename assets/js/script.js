b7a67b8b367fbf0efc791b6013a804e72464049c
iamtheasad <iamtheasad@gmail.com>
2019-12-13 23:05:21 +0600

    portfolio page added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 // for slider
-jQuery(document).ready(function(){
-   $('.homepage_slides').owlCarousel({
-      nav: true,
-      items: 1,
-      loop: true,
-      navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
-      autoplay: true,
-      dots: true,
+jQuery(document).ready(function () {
+  $('.homepage_slides').owlCarousel({
+    nav: true,
+    items: 1,
+    loop: true,
+    navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
+    autoplay: true,
+    dots: true,
   });
 
   // owlCarousel for our team section
@@ -40,30 +40,57 @@ jQuery(document).ready(function(){
 });
 
 // for circle progress bar
-jQuery(document).ready(function(){
-   $('#webdesign_skillbar').circleProgress({
-      value: 0.90,
-      size: 270,
-      fill: '#0bbbc1',
-      thickness: 4,
-    }).on('circle-animation-progress', function(event, progress) {
-      $(this).find('.skill_count_find').html(Math.round(90 * progress) + '<i>%</i>');
-    });
-    $('#graphicdesign_skillbar').circleProgress({
-      value: 0.95,
-      size: 270,
-      fill: '#0bbbc1',
-      thickness: 4,
-    }).on('circle-animation-progress', function(event, progress) {
-      $(this).find('.skill_count_find').html(Math.round(95 * progress) + '<i>%</i>');
-    });
-    $('#digitalmarketing_skillbar').circleProgress({
-      value: 0.84,
-      size: 270,
-      fill: '#0bbbc1',
-      thickness: 4,
-    }).on('circle-animation-progress', function(event, progress) {
-      $(this).find('.skill_count_find').html(Math.round(84 * progress) + '<i>%</i>');
-    });
+jQuery(document).ready(function () {
+  $('#webdesign_skillbar').circleProgress({
+    value: 0.90,
+    size: 270,
+    fill: '#0bbbc1',
+    thickness: 4,
+  }).on('circle-animation-progress', function (event, progress) {
+    $(this).find('.skill_count_find').html(Math.round(90 * progress) + '<i>%</i>');
+  });
+  $('#graphicdesign_skillbar').circleProgress({
+    value: 0.95,
+    size: 270,
+    fill: '#0bbbc1',
+    thickness: 4,
+  }).on('circle-animation-progress', function (event, progress) {
+    $(this).find('.skill_count_find').html(Math.round(95 * progress) + '<i>%</i>');
+  });
+  $('#digitalmarketing_skillbar').circleProgress({
+    value: 0.84,
+    size: 270,
+    fill: '#0bbbc1',
+    thickness: 4,
+  }).on('circle-animation-progress', function (event, progress) {
+    $(this).find('.skill_count_find').html(Math.round(84 * progress) + '<i>%</i>');
+  });
 });
 
+// Fot isotop
+jQuery(document).ready(function () {
+
+  $('.portfolio_list').isotope({
+    layoutMode: 'masonry',
+    masonry: {
+      columnWidth: '.col-md-3',
+      horizontalOrder: true
+    }
+  });
+
+  $('.portfolio_filter li').on('click', function () {
+    $('.portfolio_filter li').removeClass("portfolio_active");
+    $(this).addClass('portfolio_active');
+
+    var filterValue = $(this).attr('data-filter');
+
+    $('.portfolio_list').isotope({
+      filter: filterValue,
+      layoutMode: 'masonry',
+      masonry: {
+        columnWidth: '.col-md-3',
+        horizontalOrder: true
+      }
+    });
+  });
+});
