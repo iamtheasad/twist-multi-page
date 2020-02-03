c85035b6f7889ed7d2f8c139705d052665ed903d
iamtheasad <iamtheasad@gmail.com>
2020-02-03 21:15:58 +0600

    our team slider issues fixed & css validator issues fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,35 +47,40 @@
     };
 
 
-    // Slick Carousel for Team Section
-    $('.theme_2').slick({
-      dots: true,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      infinite: true,
-      autoplay: true,
-      arrows: false,
-      autoplaySpeed: 2000,
-      responsive: [{
+   // Slick Carousel for Team Section
+  $('.theme_2').slick({
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 2000,
+    responsive: [{
         breakpoint: 1000,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
         }
-      }, {
+      },
+      {
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
         }
-      }, {
+      },
+      {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
         }
-      }]
-    });
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+    ]
+  });
 
 
     // Slick Carousel for our testimonial
